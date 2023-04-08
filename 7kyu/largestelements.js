c84f0b3b497f1a9c6ac53ec20e992cf9143c5b72
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-04-08 18:01:26 +0300

    added another one

---
+++ 7kyu/largestelements.js
@@ -0,0 +1,16 @@
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// // => [6,7]
+
+function largest(n, array) {
+  if (n === 0){
+    return []
+  }
+  else {
+  array.sort((a,b) => a-b)
+  largestNumbers = array.slice(-n)
+  return largestNumbers;}
+}
