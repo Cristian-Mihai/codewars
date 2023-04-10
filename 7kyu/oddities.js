31a76d06b205fc484181cab2fee7c2f24e9c4174
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-04-10 17:03:32 +0300

    another challenge SRL

---
+++ 7kyu/oddities.js
@@ -0,0 +1,14 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds( values ){
+  const oddOnes = []
+ for(i=0; i<= values.length - 1; i++){
+   if(values[i] % 2 === 0) {
+     oddOnes.push(values[i])
+   }
+   else { continue}
+ }
+  return oddOnes
+}
