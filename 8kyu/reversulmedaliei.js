181583b2d1b2aa2b0aabc1db85bf8a00f1f98c37
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-05-06 17:44:13 +0300

    new clng

---
+++ 8kyu/reversulmedaliei.js
@@ -0,0 +1,8 @@
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  const strSplit = str.split('')
+  const reversedArr = strSplit.reverse()
+  return reversedArr.join('')
+}
