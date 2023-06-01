94f65cce018db617b236128fb0dfc0db76fb99c7
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-06-01 17:14:53 +0300

    added alternative sollution

--- 7kyu/trimmingastring.js
+++ 7kyu/trimmingastring.js
@@ -32,3 +32,14 @@ function trim(str, size) {
   return str;
 }
 
+// alternative
+
+function trim ( str, size ) {
+  if ( str.length <= size ) {
+    return str;
+  }
+  
+  const real = str.slice(0, size);
+  
+  return `${real.length < 4 ? real : real.slice(0, -3)}...`;
+}
