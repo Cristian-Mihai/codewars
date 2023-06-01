e4097748d02ea6b091c7820567418d8a30c27275
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-06-01 17:15:00 +0300

    added alternative sollution

--- 7kyu/findallnonconsecutive.js
+++ 7kyu/findallnonconsecutive.js
@@ -31,3 +31,15 @@ function allNonConsecutive (arr) {
   }
   else{return obj}
 }
+
+// alternative
+
+function allNonConsecutive (arr) {
+  let result = [];
+  arr.forEach((val, index) => {
+      if (val !== arr[index - 1] + 1 && index) {
+          result.push({i: index, n: val});
+      }
+  });
+  return result;
+}
