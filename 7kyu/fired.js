846fe325fc259f91f46c663380562b8cb01683e8
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-06-03 18:03:26 +0300

    added alternative solution

--- 7kyu/fired.js
+++ 7kyu/fired.js
@@ -27,3 +27,11 @@ function outed(meet, boss){
   }
 
   // :)
+
+  // alternative
+
+  function outed(meet, boss) {
+    let names = Object.keys(meet)
+    let score = names.reduce((s,v) => s + meet[v], 0) + meet[boss]
+    return score / names.length > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
+  }
