c669b3b879567a5f0343028f4e0096df2d52cde5
Cristian-Mihai <cmihai.webdev@gmail.com>
2023-06-03 18:03:13 +0300

    added alternative solution

--- 7kyu/endswith.js
+++ 7kyu/endswith.js
@@ -10,3 +10,13 @@ function solution(str, ending){
 }
 
 // :)
+
+// alternative
+
+function solution(str, ending){
+  if (typeof(str) != "string" || typeof(ending) != "string")
+    throw "wrong type";
+  if (ending.length>str.length)
+    return false;
+  return str.substr(str.length-ending.length, ending.length) == ending;
+}
